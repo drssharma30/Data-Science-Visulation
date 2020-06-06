ed675c768480b0fcbf1bced9fc2e6c1a07ae6289
Shweta Sharma <drssharma30@gmail.com>
2020-06-06 16:19:13 -0500

    Assesment Distributions Exercise 1. Distributions Exercise 2. Distributions Exercise 3. Empirical Cumulative Distribution Function (eCDF) Exercise 4. eCDF Male Heights Exercise 5. eCDF of Murder Rates Exercise 6. eCDF of Murder Rates Exercise 7. Histograms Exercise 8. Histograms Exercise 9. Density plots Exercise 10. Density plots - 2

---
+++ Distributions.R
@@ -0,0 +1,94 @@
+#Exercise 1. Distributions - 1
+#In the murders dataset, the region is a categorical variable and on the right you can see its distribution. To the closet 5%, what proportion of the states are in the North Central region?
+# Ans 25%
+
+
+
+
+
+#Exercise 2. Distributions - 2
+#In the murders dataset, the region is a categorical variable and to the right is its distribution.
+
+#Which of the following is true:
+# Ans The graph shows only four numbers with a bar plot.
+
+
+
+
+# Exercise 3. Empirical Cumulative Distribution Function (eCDF)
+#The plot shows the eCDF for male heights:
+  
+ # Based on the plot, what percentage of males are shorter than 75 inches?
+# Ans 95%
+
+
+
+
+
+# Exercise 4. eCDF Male Heights
+#The plot shows the eCDF for male heights:
+  
+ # To the closest inch, what height m has the property that 1/2 of the male students are taller than m and 1/2 are shorter?
+# Ans 69 inches
+
+
+
+
+
+
+#Exercise 5. eCDF of Murder Rates
+#Here is an eCDF of the murder rates across states.
+
+#Knowing that there are 51 states (counting DC) and based on this plot, how many states have murder rates larger than 10 per 100,000 people?
+# Ans 1
+
+
+
+
+
+#Exercise 6. eCDF of Murder Rates - 2
+#Here is an eCDF of the murder rates across states:
+  
+ # Based on the eCDF above, which of the following statements are true:
+# With the exception of 4 states, the murder rates are below 5 per 100,000.
+
+
+
+
+
+
+#Exercise 7. Histograms
+#Here is a histogram of male heights in our heights dataset:
+  
+ # Based on this plot, how many males are between 62.5 and 65.5?
+# Ans 58
+
+
+
+
+
+#Exercise 8. Histograms - 2
+#Here is a histogram of male heights in our heights dataset:
+  
+ # About what percentage are shorter than 60 inches?
+# Ans 1%
+
+
+
+
+
+#Exercise 9. Density plots
+#Based on this density plot, about what proportion of US states have populations larger than 10 million?
+# Ans 0.15
+
+
+
+
+
+#Exercise 10. Density plots - 2
+#Here are three density plots. Is it possible that they are from the same dataset? Which of the following statements is true:
+#They are the same dataset, but the first does not have the x-axis in the log scale, the second undersmooths and the third oversmooths.
+
+
+
+
